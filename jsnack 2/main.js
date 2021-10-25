9cc43bc0444aa0ed5a4a530f3e3c0e12e6654b7f
pater97 <giuseppepater997@gmail.com>
2021-10-25 12:20:11 +0200

    creazione array

---
+++ jsnack 2/main.js
@@ -0,0 +1,42 @@
+//CONSEGNA
+/*
+Descrizione:
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti e falli subiti.
+Nome sarà l’unica proprietà da compilare, mentre le altre saranno tutte settate a 0.
+*/
+
+//creazione array
+
+const campionato = [
+    {
+        squadra: `inter`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        squadra: `milan`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        squadra: `juventus`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        squadra: `atalanta`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        squadra: `napoli`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    },
+    {
+        squadra: `roma`,
+        puntiFatti: 0,
+        falliSubiti: 0
+    }
+]
